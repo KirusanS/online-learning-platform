886a179cb2139dbe1b2ba90ad72f7c023a52fdf9
Kirusan Subeethakumar <35615074+KirusanS@users.noreply.github.com>
2020-12-29 22:54:56 -0500

    Updated from browser router to hash router

--- src/react/src/App.js
+++ src/react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import logo from './assets/uimpactify-logo.png';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Sidebar from "./components/sidebar/sidebar.component";
 
 
