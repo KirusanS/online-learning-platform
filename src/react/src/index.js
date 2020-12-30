ed10c4d027b58d49be68908e3f82efb2d637a8a9
Kirusan Subeethakumar <35615074+KirusanS@users.noreply.github.com>
2020-12-29 22:56:48 -0500

    Update index.js

--- src/react/src/index.js
+++ src/react/src/index.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Sidebar from "./components/sidebar/sidebar.component.js";
