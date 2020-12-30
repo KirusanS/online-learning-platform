fee458ecd72f50def6f4e3b81d20611bf91ab2bd
Kirusan Subeethakumar <35615074+KirusanS@users.noreply.github.com>
2020-12-29 22:15:13 -0500

    Updates

---
+++ precache-manifest.63d6576f2cf9908fe7e783e80c740a35.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288192d1c84908539d017beeec303998",
+    "url": "/online-learning-platform/index.html"
+  },
+  {
+    "revision": "1ce8aa9366d4565bfbe1",
+    "url": "/online-learning-platform/static/css/2.705119b7.chunk.css"
+  },
+  {
+    "revision": "bab094e8547ac6b39c91",
+    "url": "/online-learning-platform/static/css/main.2341808d.chunk.css"
+  },
+  {
+    "revision": "1ce8aa9366d4565bfbe1",
+    "url": "/online-learning-platform/static/js/2.82d99792.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/online-learning-platform/static/js/2.82d99792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab094e8547ac6b39c91",
+    "url": "/online-learning-platform/static/js/main.a85bcf2b.chunk.js"
+  },
+  {
+    "revision": "318c3522e8124c4fd218",
+    "url": "/online-learning-platform/static/js/runtime-main.6e85ef64.js"
+  },
+  {
+    "revision": "b4374b51f1ca25f69f9aa5688d6c7a00",
+    "url": "/online-learning-platform/static/media/backgroundAbout.b4374b51.svg"
+  },
+  {
+    "revision": "7e22cd67e6a5cde71191472b47f810eb",
+    "url": "/online-learning-platform/static/media/backgroundHome.7e22cd67.svg"
+  },
+  {
+    "revision": "34d406388bcdc8be080c1298651ea181",
+    "url": "/online-learning-platform/static/media/backgroundgradient.34d40638.png"
+  },
+  {
+    "revision": "e2dc2b062d14e03cf9a69c631cbb554a",
+    "url": "/online-learning-platform/static/media/createPicture.e2dc2b06.png"
+  },
+  {
+    "revision": "4a596654158fccfbff002e0e9c95c25b",
+    "url": "/online-learning-platform/static/media/fireworks.4a596654.png"
+  },
+  {
+    "revision": "791c7e6694002b30467bc20ed56e4c8f",
+    "url": "/online-learning-platform/static/media/getStartedButton.791c7e66.png"
+  },
+  {
+    "revision": "ffc4ad950cd414a41ad4546542094e5b",
+    "url": "/online-learning-platform/static/media/instructorButton.ffc4ad95.svg"
+  },
+  {
+    "revision": "3216665288fdf630ba627833f98ae0e7",
+    "url": "/online-learning-platform/static/media/laptoplady.32166652.png"
+  },
+  {
+    "revision": "32a55240ecceb80970ca762a4712a085",
+    "url": "/online-learning-platform/static/media/loginPicture.32a55240.png"
+  },
+  {
+    "revision": "5b3dc69139ec5b7dfe9cc549006923eb",
+    "url": "/online-learning-platform/static/media/macPicture.5b3dc691.svg"
+  },
+  {
+    "revision": "b48821b57235238221460412fecb2db0",
+    "url": "/online-learning-platform/static/media/macPicture2.b48821b5.png"
+  },
+  {
+    "revision": "3e0aaab7fd9525b92517e415a56059d1",
+    "url": "/online-learning-platform/static/media/pricing.3e0aaab7.PNG"
+  },
+  {
+    "revision": "f5db4071b74b294821f4937472fcb7f5",
+    "url": "/online-learning-platform/static/media/profilePic.f5db4071.png"
+  },
+  {
+    "revision": "a80fe57545ea9dfe2b6a946c229bf5f6",
+    "url": "/online-learning-platform/static/media/profilePicBackup.a80fe575.png"
+  },
+  {
+    "revision": "31cab93ae645f31dca1b49b583f9ee4a",
+    "url": "/online-learning-platform/static/media/rsz_1backgroundgradient.31cab93a.png"
+  },
+  {
+    "revision": "a93ab5942bb93d49c848756c4ce6739e",
+    "url": "/online-learning-platform/static/media/rsz_backgroundgradient.a93ab594.png"
+  },
+  {
+    "revision": "11563960182c61ee3de6289bf411c019",
+    "url": "/online-learning-platform/static/media/socIn1.11563960.svg"
+  },
+  {
+    "revision": "7b51ad4b22b93824872b2e0fc7fd8a58",
+    "url": "/online-learning-platform/static/media/socIn10.7b51ad4b.svg"
+  },
+  {
+    "revision": "7f48cbc464f2b5ef9812cbe733e607ec",
+    "url": "/online-learning-platform/static/media/socIn11.7f48cbc4.svg"
+  },
+  {
+    "revision": "730aff91400ff92a7efe1ad93a5f1305",
+    "url": "/online-learning-platform/static/media/socIn12.730aff91.svg"
+  },
+  {
+    "revision": "a7f55010a4038cd8ae23fb5ab8390efb",
+    "url": "/online-learning-platform/static/media/socIn2.a7f55010.svg"
+  },
+  {
+    "revision": "d7678e38ab9b09a8b863f53a2731dbbf",
+    "url": "/online-learning-platform/static/media/socIn3.d7678e38.svg"
+  },
+  {
+    "revision": "457ebdb36eb8d08e86fc6aead25b6427",
+    "url": "/online-learning-platform/static/media/socIn4.457ebdb3.svg"
+  },
+  {
+    "revision": "a614cecb67b4d2aac77cd35f512d630a",
+    "url": "/online-learning-platform/static/media/socIn5.a614cecb.svg"
+  },
+  {
+    "revision": "d23f38f2e76bbb61717aff656e8ddc69",
+    "url": "/online-learning-platform/static/media/socIn6.d23f38f2.svg"
+  },
+  {
+    "revision": "d814d59be10b80363d61873d1d019dd1",
+    "url": "/online-learning-platform/static/media/socIn7.d814d59b.svg"
+  },
+  {
+    "revision": "ddcfc6d837d97afd67cac4d7cbdcfe45",
+    "url": "/online-learning-platform/static/media/socIn8.ddcfc6d8.svg"
+  },
+  {
+    "revision": "f92c6e805a8bed2cfc49752e533b669a",
+    "url": "/online-learning-platform/static/media/socIn9.f92c6e80.svg"
+  },
+  {
+    "revision": "bb3f99bb69c45462cb5e2af4190e0a5b",
+    "url": "/online-learning-platform/static/media/solutionsComponent.bb3f99bb.svg"
+  },
+  {
+    "revision": "73bd1bdf3a84ff9f10796a31d68428e3",
+    "url": "/online-learning-platform/static/media/solutionsInstructor.73bd1bdf.svg"
+  },
+  {
+    "revision": "85d6dd3daf67ca7012c6cb748b0caa68",
+    "url": "/online-learning-platform/static/media/solutionsStudent.85d6dd3d.svg"
+  },
+  {
+    "revision": "8079dc20f0d35b3e6e7d4b5afc94f4da",
+    "url": "/online-learning-platform/static/media/studentButton.8079dc20.svg"
+  },
+  {
+    "revision": "1f35fea4a5e4606ab57d00f1ff3d51de",
+    "url": "/online-learning-platform/static/media/teacherInClassSittingDown.1f35fea4.svg"
+  },
+  {
+    "revision": "d5bf02ae8b0006a24d6eb203dcab0259",
+    "url": "/online-learning-platform/static/media/teacherInClassStandingUp.d5bf02ae.svg"
+  },
+  {
+    "revision": "51e2087372bc55cccca82d93cee43acb",
+    "url": "/online-learning-platform/static/media/testimonials.51e20873.svg"
+  },
+  {
+    "revision": "fce1522375fd809f7de81d054645f15a",
+    "url": "/online-learning-platform/static/media/uimpactify-logo.fce15223.png"
+  },
+  {
+    "revision": "2597d50a531e67960f4f7d29d6ce5b6e",
+    "url": "/online-learning-platform/static/media/undraw_questions.2597d50a.png"
+  },
+  {
+    "revision": "9d6bdb5e88535ae6f46173db77dfd53f",
+    "url": "/online-learning-platform/static/media/whoWeServe.9d6bdb5e.svg"
+  },
+  {
+    "revision": "ba79b504ec2422fcb0bba0c87d367db7",
+    "url": "/online-learning-platform/static/media/whyUImpactify.ba79b504.svg"
+  }
+]);
